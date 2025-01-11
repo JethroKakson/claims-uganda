f9ac6c7947c76a1c4972cd6553513f422295ac93
Elijah <mukisaelijah293@gmail.com>
2025-01-11 12:55:48 +0300

    Test User Login

--- Auth/tests.py
+++ Auth/tests.py
@@ -4,6 +4,7 @@ from Staff.models import Staff
 from django.urls import reverse
 from allauth.socialaccount.models import SocialApp
 from claims.settings import SOCIAL_AUTH_GOOGLE_CLIENT_ID, SOCIAL_AUTH_GOOGLE_SECRET
+from django.contrib.messages import get_messages
 
 class LoginUserTestCase(TestCase):
     def setUp(self):
@@ -22,14 +23,14 @@ class LoginUserTestCase(TestCase):
         user.save()
         response = self.client.post(reverse('login'), {'username': 'test', 'password': 'password'})
         self.assertRedirects(response, reverse('login'))
-        messages = list(response.context.get('messages'))
+        messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Your account is inactive. Please contact the admin.')
 
     def test_login_user_not_authenticated(self):
         response = self.client.post(reverse('login'), {'username': 'test', 'password': 'wrongpassword'})
         self.assertRedirects(response, reverse('login'))
-        messages = list(response.context.get('messages'))
+        messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Invalid username or password.')
 
