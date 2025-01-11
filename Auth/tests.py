9234c0b3323ab9bcaf43c131f3d163d70989294f
Elijah <mukisaelijah293@gmail.com>
2025-01-11 12:28:44 +0300

    Test User Login

--- Auth/tests.py
+++ Auth/tests.py
@@ -17,6 +17,7 @@ class LoginUserTestCase(TestCase):
         self.assertRedirects(response, reverse('dashboard'))
 
     def test_login_user_inactive(self):
+        SocialApp.objects.create(provider='google', name='google', client_id=SOCIAL_AUTH_GOOGLE_CLIENT_ID, secret=SOCIAL_AUTH_GOOGLE_SECRET)
         user = User.objects.create_user('test', 'test@example.com', 'password')
         user.staff = Staff.objects.create(status='Inactive', user=user)
         user.save()
@@ -25,16 +26,13 @@ class LoginUserTestCase(TestCase):
         self.assertEqual(response.context['messages']._loaded_messages[0].message, 'Your account is inactive. Please contact the admin.')
 
     def test_login_user_not_authenticated(self):
+        SocialApp.objects.create(provider='google', name='google', client_id=SOCIAL_AUTH_GOOGLE_CLIENT_ID, secret=SOCIAL_AUTH_GOOGLE_SECRET)
         response = self.client.post(reverse('login'), {'username': 'test', 'password': 'wrongpassword'})
         self.assertRedirects(response, reverse('login'))
         self.assertEqual(response.context['messages']._loaded_messages[0].message, 'Invalid username or password.')
 
     def test_login_user_next_param(self):
+        SocialApp.objects.create(provider='google', name='google', client_id=SOCIAL_AUTH_GOOGLE_CLIENT_ID, secret=SOCIAL_AUTH_GOOGLE_SECRET)
         response = self.client.get(reverse('login'), {'next': reverse('dashboard')})
         self.assertContains(response, "Your session has expired. Please log in again.")
-
-    def test_social_login(self):
-        response = self.client.get(reverse('socialaccount_login', args=['google']))
-        self.assertContains(response, "Google")
-        self.assertContains(response, "Login with Google")
 
