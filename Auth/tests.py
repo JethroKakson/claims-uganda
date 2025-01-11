a7ee6b5999f4f31f193c00284857ef0cf2f2794d
Elijah <mukisaelijah293@gmail.com>
2025-01-11 11:49:47 +0300

    Test User Login

--- Auth/tests.py
+++ Auth/tests.py
@@ -2,8 +2,12 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from Staff.models import Staff
 from django.urls import reverse
+from allauth.socialaccount.models import SocialApp
 
 class LoginUserTestCase(TestCase):
+    def setUp(self):
+        self.social_app = SocialApp.objects.create(provider='google', name='google', client_id='1234567890', secret='1234567890')
+
     def test_login_user_active(self):
         user = User.objects.create_user('test', 'test@example.com', 'password')
         user.staff = Staff.objects.create(status='Active', user=user)
@@ -27,3 +31,9 @@ class LoginUserTestCase(TestCase):
     def test_login_user_next_param(self):
         response = self.client.get(reverse('login'), {'next': reverse('dashboard')})
         self.assertContains(response, "Your session has expired. Please log in again.")
+
+    def test_social_login(self):
+        response = self.client.get(reverse('socialaccount_login', args=['google']))
+        self.assertContains(response, "Google")
+        self.assertContains(response, "Login with Google")
+
