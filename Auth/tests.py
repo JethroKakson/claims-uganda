c5b54a20463bfd0561145c99521dac394cefe31b
Elijah <mukisaelijah293@gmail.com>
2025-01-11 11:56:59 +0300

    Test User Login

--- Auth/tests.py
+++ Auth/tests.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from Staff.models import Staff
 from django.urls import reverse
 from allauth.socialaccount.models import SocialApp
-from claims.settings import client_id, client_secret
+from claims.settings import SOCIAL_AUTH_GOOGLE_CLIENT_ID, SOCIAL_AUTH_GOOGLE_SECRET
 
 class LoginUserTestCase(TestCase):
     def setUp(self):
-        self.social_app = SocialApp.objects.create(provider='google', name='google', client_id=client_id, secret=client_secret)
+        self.social_app = SocialApp.objects.create(provider='google', name='google', client_id=SOCIAL_AUTH_GOOGLE_CLIENT_ID, secret=SOCIAL_AUTH_GOOGLE_SECRET)
 
     def test_login_user_active(self):
         user = User.objects.create_user('test', 'test@example.com', 'password')
