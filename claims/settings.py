8bd6ef721c63465cb427d279f8af7494f50bad19
JethroKakson <jethrokakson@gmail.com>
2025-01-15 14:20:32 +0300

    Report fixes

--- claims/settings.py
+++ claims/settings.py
@@ -110,7 +110,12 @@ WSGI_APPLICATION = 'claims.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
     'default': {
@@ -126,12 +131,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 SITE_ID = 1
